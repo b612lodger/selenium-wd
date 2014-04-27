5b17cb5d263c868a055c1dc82c05a648bd7ecf28
Saint-Exupéry <b612lodger@gmail.com>
2014-04-28 04:17:51 +0900

    selenium-wd done callback now have err.

--- test/selenium-wd-test.js
+++ test/selenium-wd-test.js
@@ -7,6 +7,18 @@ var expect = expect || require('expect.js');
 describe('SeleniumWD', function () {
 	var SeleniumWD = require('../lib/selenium-wd');
 	describe('startTest()', function () {
+
+		it('should start local selenium if SeleniumWD is initialized with localhost remote', function (done) {
+			var seleniumWd = new SeleniumWD({url: 'localhost'}, [{browserName: 'safari'}]);
+			seleniumWd.startTest('async', function (browser) {
+				browser.quit();
+			}, function (err) {
+				expect(err).to.not.be.ok();
+			});
+			done();
+		});
+
+
 		it('should call test callback and browser should not be null', function (done) {
 			var seleniumWd = new SeleniumWD();
 			seleniumWd.startTest('async', function (browser) {
@@ -22,7 +34,7 @@ describe('SeleniumWD', function () {
 			var seleniumWd = new SeleniumWD();
 			seleniumWd.startTest('async', function (browser) {
 				browser.quit();
-			}, function () {
+			}, function (err) {
 				done();
 			});
 		});
